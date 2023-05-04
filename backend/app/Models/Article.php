ec01a3f7f8eca75a6a4c23e736a5cd3e3e8b17c3
Zoren <zorenleolumosbog@gmail.com>
2023-05-04 16:18:10 +0800

    Create user, company and article migration and model

---
+++ backend/app/Models/Article.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'image',
+        'title',
+        'date',
+        'content',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'id' => 'string',
+        'date' => 'datetime',
+    ];
+}
