ae9aa6a6099ab376aff42a23cb03c682769968be
Zoren <zorenleolumosbog@gmail.com>
2023-05-09 23:26:34 +0800

    Add table responsiveness

--- frontend/src/views/TheDashboard.vue
+++ frontend/src/views/TheDashboard.vue
@@ -12,7 +12,7 @@ onMounted(() => {
         <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
           <div class="mdc-card table-responsive">
             <div class="table-heading px-2 px-1 border-bottom">
-              <h1 class="mdc-card__title mdc-card__title--large">Employee status</h1>
+              <h1 class="mdc-card__title mdc-card__title--large">For Edit</h1>
             </div>
             <table class="table">
               <thead>
@@ -62,8 +62,37 @@ onMounted(() => {
                   <td>278</td>
                   <td><div class="col mdc-button" data-mdc-auto-init="MDCRipple"><i class="mdi mdi-heart text-blue"></i></div><div class="col mdc-button" data-mdc-auto-init="MDCRipple"><i class="mdi mdi-forum text-yellow"></i></div><div class="col mdc-button" data-mdc-auto-init="MDCRipple"><i class="mdi mdi-delete text-red"></i></div></td>
                 </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mdc-layout-grid">
+      <div class="mdc-layout-grid__inner">
+        <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
+          <div class="mdc-card table-responsive">
+            <div class="table-heading px-2 px-1 border-bottom">
+              <h1 class="mdc-card__title mdc-card__title--large">Published</h1>
+            </div>
+            <table class="table">
+              <thead>
                 <tr>
-                  <td class="text-left">Gloves</td>
+                  <th class="text-left">Product</th>
+                  <th>Cost</th>
+                  <th>Sales amount</th>
+                  <th>Shipping cost</th>
+                  <th>Units sold</th>
+                  <th>Profit generated</th>
+                  <th>Left in stock</th>
+                  <th>Returns</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class="text-left">T-shirts</td>
                   <td>250</td>
                   <td>300</td>
                   <td>60</td>
@@ -74,18 +103,29 @@ onMounted(() => {
                   <td><div class="col mdc-button" data-mdc-auto-init="MDCRipple"><i class="mdi mdi-heart text-blue"></i></div><div class="col mdc-button" data-mdc-auto-init="MDCRipple"><i class="mdi mdi-forum text-yellow"></i></div><div class="col mdc-button" data-mdc-auto-init="MDCRipple"><i class="mdi mdi-delete text-red"></i></div></td>
                 </tr>
                 <tr>
-                  <td class="text-left">Shoes</td>
-                  <td>673</td>
+                  <td class="text-left">Baseball Hat</td>
                   <td>457</td>
-                  <td>56</td>
-                  <td>4467</td>
-                  <td>98</td>
-                  <td>345723</td>
-                  <td>350</td>
+                  <td>204</td>
+                  <td>35</td>
+                  <td>6754</td>
+                  <td>35</td>
+                  <td>345623</td>
+                  <td>546</td>
                   <td><div class="col mdc-button" data-mdc-auto-init="MDCRipple"><i class="mdi mdi-heart text-blue"></i></div><div class="col mdc-button" data-mdc-auto-init="MDCRipple"><i class="mdi mdi-forum text-yellow"></i></div><div class="col mdc-button" data-mdc-auto-init="MDCRipple"><i class="mdi mdi-delete text-red"></i></div></td>
                 </tr>
-                </tbody>
-              </table>
+                <tr>
+                  <td class="text-left">Tennis Racket</td>
+                  <td>250</td>
+                  <td>350</td>
+                  <td>38</td>
+                  <td>3289</td>
+                  <td>45</td>
+                  <td>54662</td>
+                  <td>278</td>
+                  <td><div class="col mdc-button" data-mdc-auto-init="MDCRipple"><i class="mdi mdi-heart text-blue"></i></div><div class="col mdc-button" data-mdc-auto-init="MDCRipple"><i class="mdi mdi-forum text-yellow"></i></div><div class="col mdc-button" data-mdc-auto-init="MDCRipple"><i class="mdi mdi-delete text-red"></i></div></td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
